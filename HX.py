53c56111b8d3e216b2c0dcc97c43482ba051459f
XV333 <141644390+XV333@users.noreply.github.com>
2023-08-07 16:59:04 +0300

    Update and rename Cp.py to HX.py
    Hhhh

--- HX.py
+++ HX.py
@@ -1,6 +1,6 @@
 printf "\e[1;36mWelcome to my tool (pkg.Termux), you can install the basic commands you need Termux and solve all the problems that you will face when installing any tool"
 printf "\n"
-printf "\e[1;36
+printf "\e[1;37
                   ███╗░░░███╗░██████╗
                   ████╗░████║██╔════╝
                   ██╔████╔██║╚█████╗░
@@ -30,7 +30,101 @@ pkg install python3 -y
 pkg install perl
 pkg install ruby
 pkg install wget
-git clone https://github.com/MFDGaming/ubuntu-in-termux
-cd ~
+
+apt update
+
+apt upgrade
+
+apt-get update
+
+apt-get upgrade
+
+apt install php
+
+apt install python
+
+apt install python2
+
+apt install git
+install nano
+
+pkg install figlet
+
+pkg install wget
+
+pkg install python2
+
+pkg install python2-dev
+
+pkg install cowsay
+
+pkg install toilet
+
+pkg install ruby
+
+pkg install help
+
+gem install lolcat
+
+pkg install curl
+
+pkg install wgetrc
+
+pkg install unzip
+
+pkg install openssh
+
+pkg install tor
+
+pkg install tar
+
+pkg install zip
+
+pkg install net-tools
+
+pkg install unrar
+
+pkg install clang
+
+pkg install w3m
+
+pkg install proot
+
+pip2 install wget
+
+pip2 install requests
+apt update
+apt upgrade
+apt install git
+apt install Python
+
+apt install Python2
+
+apt installe Python3
+
+apt install nano
+
+apt install Python
+ pip apt 
+install ruby
+
+pip install bundle
+
+apt-get install gcc
+
+apt-get install Build- essential
+
+apt-get install perl
+
+apt-get install git
+
+apt-get install libany-uri-escape-perl
+
+apt-get install libhtmal-html5-entites-numbered-perl
+
+apt-get install lbhtmal-parser-perl
+
+apt-get install libwww-perl
+
 printf "\e[1;36m Termux Install Finished .. you can following more in my Channel and pages (MS TECH) 
 printf "\n"
