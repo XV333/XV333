b0c49e15a40c2ed0462484b8154e6a6dc5f15064
XV333 <141644390+XV333@users.noreply.github.com>
2023-08-07 16:46:09 +0300

    Create Cp.py
    Jjj

---
+++ Cp.py
@@ -0,0 +1,36 @@
+printf "\e[1;36mWelcome to my tool (pkg.Termux), you can install the basic commands you need Termux and solve all the problems that you will face when installing any tool"
+printf "\n"
+printf "\e[1;36
+                  ███╗░░░███╗░██████╗
+                  ████╗░████║██╔════╝
+                  ██╔████╔██║╚█████╗░
+                  ██║╚██╔╝██║░╚═══██╗
+                  ██║░╚═╝░██║██████╔╝
+                  ╚═╝░░░░░╚═╝╚═════╝░
+
+           ████████╗███████╗░█████╗░██╗░░██╗
+           ╚══██╔══╝██╔════╝██╔══██╗██║░░██║
+           ░░░██║░░░█████╗░░██║░░╚═╝███████║
+           ░░░██║░░░██╔══╝░░██║░░██╗██╔══██║
+           ░░░██║░░░███████╗╚█████╔╝██║░░██║
+           ░░░╚═╝░░░╚══════╝░╚════╝░╚═╝░░╚═╝"
+printf "\n"
+printf "\e[1;32m"
+pkg install root-repo -y
+termux-setup-storage -y
+pkg update -y
+pkg upgrade -y
+pkg install php -y
+pkg install openssh -y
+pkg install github -y
+pkg install figlet -y
+pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+pkg install perl
+pkg install ruby
+pkg install wget
+git clone https://github.com/MFDGaming/ubuntu-in-termux
+cd ~
+printf "\e[1;36m Termux Install Finished .. you can following more in my Channel and pages (MS TECH) 
+printf "\n"
